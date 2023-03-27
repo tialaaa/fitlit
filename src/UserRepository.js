d1463304022227e89e02ef1604b8dd996facc62d
Sara Park <sspark106@gmail.com>
2023-03-27 17:20:32 -0600

    feat: Add test for method to calculate average step goal

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,12 +1,12 @@
 class UserRepository {
-    constructor(id, name, address, email, strideLength, dailyStepGoal, friends) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.email = email;
-        this.strideLength = strideLength;
-        this.dailyStepGoal = dailyStepGoal;
-        this.friends = friends;
+    constructor(arrayOfUsers) {
+        this.usersData = arrayOfUsers;
+    }
+
+    findUser(id) {
+        return this.usersData.find(user => {
+            return user.id === id;
+        })
     }
 }
 
--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -2,9 +2,49 @@ import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
 
 describe('User Repository', () => {
-  let user;
+  let users;
   beforeEach(() => {
-    user = new UserRepository(1, "Trystan Gorczany", "9484 Lucas Flat, West Kittymouth WA 67504", "Taurean_Pollich31@gmail.com", 4, 7000, [5, 43, 46, 11]);
+    users = new UserRepository([{
+      "id": 1,
+      "name": "Trystan Gorczany",
+      "address": "9484 Lucas Flat, West Kittymouth WA 67504",
+      "email": "Taurean_Pollich31@gmail.com",
+      "strideLength": 4,
+      "dailyStepGoal": 7000,
+      "friends": [
+        5,
+        43,
+        46,
+        11
+      ]
+    },
+    {
+      "id": 2,
+      "name": "Tyreek VonRueden",
+      "address": "623 Koelpin Skyway, Lake Luigichester MN 77576-1678",
+      "email": "Nicolette_Halvorson43@yahoo.com",
+      "strideLength": 4.5,
+      "dailyStepGoal": 9000,
+      "friends": [
+        13,
+        19,
+        3
+      ]
+    },
+    {
+      "id": 3,
+      "name": "Colt Rohan",
+      "address": "48010 Balistreri Harbor, Cleobury IN 43317",
+      "email": "Wilford.Barton@gmail.com",
+      "strideLength": 2.7,
+      "dailyStepGoal": 3000,
+      "friends": [
+        31,
+        16,
+        15,
+        7
+      ]
+    }]);
   });
   
   it('should be a function', function () {
@@ -12,36 +52,72 @@ describe('User Repository', () => {
   });
 
   it('should be an instance of UserRepository', () => {
-    expect(user).to.be.an.instanceOf(UserRepository);
+    expect(users).to.be.an.instanceOf(UserRepository);
   });
 
-  it('should have a property with key of id', () => {
-    expect(user.id).to.equal(1);
+  it('should hold an array of objects', () => {
+    expect(users.usersData).to.deep.equal([{
+      "id": 1,
+      "name": "Trystan Gorczany",
+      "address": "9484 Lucas Flat, West Kittymouth WA 67504",
+      "email": "Taurean_Pollich31@gmail.com",
+      "strideLength": 4,
+      "dailyStepGoal": 7000,
+      "friends": [
+        5,
+        43,
+        46,
+        11
+      ]
+    },
+    {
+      "id": 2,
+      "name": "Tyreek VonRueden",
+      "address": "623 Koelpin Skyway, Lake Luigichester MN 77576-1678",
+      "email": "Nicolette_Halvorson43@yahoo.com",
+      "strideLength": 4.5,
+      "dailyStepGoal": 9000,
+      "friends": [
+        13,
+        19,
+        3
+      ]
+    },
+    {
+      "id": 3,
+      "name": "Colt Rohan",
+      "address": "48010 Balistreri Harbor, Cleobury IN 43317",
+      "email": "Wilford.Barton@gmail.com",
+      "strideLength": 2.7,
+      "dailyStepGoal": 3000,
+      "friends": [
+        31,
+        16,
+        15,
+        7
+      ]
+    }]);
   });
 
-  it('should have a property with key of name', () => {
-    expect(user.name).to.equal("Trystan Gorczany");
+  it('should find user data given an id', () => {
+    expect(users.findUser(1)).to.deep.equal({
+      "id": 1,
+      "name": "Trystan Gorczany",
+      "address": "9484 Lucas Flat, West Kittymouth WA 67504",
+      "email": "Taurean_Pollich31@gmail.com",
+      "strideLength": 4,
+      "dailyStepGoal": 7000,
+      "friends": [
+        5,
+        43,
+        46,
+        11
+      ]
+    });
   });
 
-  it('should have a property with key of address', () => {
-    expect(user.address).to.equal("9484 Lucas Flat, West Kittymouth WA 67504");
+  it('should calculate average step goal amongst users', () => {
+    expect(users.calcAvgStepGoal()).to.equal(6333);
   });
-
-  it('should have a property with key of email', () => {
-    expect(user.email).to.equal("Taurean_Pollich31@gmail.com");
-  });
-
-  it('should have a property with key of strideLength', () => {
-    expect(user.strideLength).to.equal(4);
-  });
-
-  it('should have a property with key of dailyStepGoal', () => {
-    expect(user.dailyStepGoal).to.equal(7000);
-  });
-
-  it('should have a property with key of friends', () => {
-    expect(user.friends).to.deep.equal([5, 43, 46, 11]);
-  });
-
   
 });
