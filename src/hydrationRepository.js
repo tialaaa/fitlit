0bbdc8f2eca5ab8b320ff31a988432fab540728f
Michael Lobato <mike.lobato.wft@gmail.com>
2023-03-27 19:59:05 -0700

    Add: method that calculates daily average for user

--- src/hydrationRepository.js
+++ src/hydrationRepository.js
@@ -4,6 +4,15 @@ class UserHydration {
     }
 
     getUserHydrationByID = (id) => this.hydrationData.filter(hydrationObj => hydrationObj.userID === id)
+
+    userHydrationAllTime = (id) => {
+        let userHydrationData = this.getUserHydrationByID(id)
+        let hydrationAverage = userHydrationData.reduce((acc, currObj) => {
+            acc += currObj.numOunces / userHydrationData.length
+            return acc
+        }, 0)
+        return Math.round(hydrationAverage)
+    }
     
 }
 
