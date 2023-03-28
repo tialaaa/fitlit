c4c291a73cd12b274468928e0005d1165b2776bb
Michael Lobato <mike.lobato.wft@gmail.com>
2023-03-27 18:35:16 -0700

    Add: testing to pass filter user by ID

--- test/hydrationRepo-test.js
+++ test/hydrationRepo-test.js
@@ -1,5 +1,6 @@
 import { expect } from "chai";
-import hydrationRepository from "../src/hydrationRepository";
+import UserHydration from "../src/hydrationRepository";
+
 
 describe("Hydration Repository", () => {
     let hydrationStats
@@ -41,10 +42,39 @@ describe("Hydration Repository", () => {
             "userID": 33,
             "date": "2023/03/30",
             "numOunces": 62
-          }]
+          },
+          {
+            "userID": 32,
+            "date": "2023/03/30",
+            "numOunces": 62
+          }];
 
-          hydrationRepo = new userHydration(hydrationStats)
+          hydrationRepo = new UserHydration(hydrationStats)
     })
+
+    it('it should be a function', function() {
+        expect(UserHydration).to.be.a('function')
+    });
+
+    it('should be an instance of UserHydration', () => {
+        expect(hydrationRepo).to.be.an.instanceOf(UserHydration)
+    })
+
+    it('should return users hydration data when providing id', () => {
+        expect(hydrationRepo.getUserHydrationByID(32)).to.deep.equal([{
+            "userID": 32,
+            "date": "2023/03/30",
+            "numOunces": 62
+          }])
+        expect(hydrationRepo.getUserHydrationByID(33).length).to.equal(7)
+    })
+
+
+
+    // it('should take in a users daily amount of ounces drank', () => {
+    //     expect(UserHydration.)
+    // })
+
 })
 
 
