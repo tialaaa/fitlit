c8a900defb084dcc656edd57fef7ac9a980b58b8
Michael Lobato <mike.lobato.wft@gmail.com>
2023-03-28 18:32:53 -0700

    Add: function that dynamically fetches api data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,41 +1,7 @@
-// Your fetch requests will live here!
-export { userData, sleepData, hydrationData, activityData }
 
-console.log('I will be a fetch request!')
+function fetchData(type) {
+    return fetch(`https://fitlit-api.herokuapp.com/api/v1/${type}`)
+    .then(res => res.json())
+}
 
-let userData =
-fetch("https://fitlit-api.herokuapp.com/api/v1/users")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        Object.values(data)[0];
-    })
-    .catch(err => console.log(err))
-
-let sleepData = 
-fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        Object.values(data)[0];
-    })
-    .catch(err => console.log(err))
-
-let hydrationData = 
-fetch("https://fitlit-api.herokuapp.com/api/v1/hydration")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        Object.values(data)[0];
-    })
-    .catch(err => console.log(err))
-
-let activityData = 
-fetch("https://fitlit-api.herokuapp.com/api/v1/activity")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        Object.values(data)[0];
-    })
-    .catch(err => console.log(err))
-
+export { fetchData }
