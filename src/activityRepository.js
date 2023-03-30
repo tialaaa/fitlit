caee19742b6fe17541cbed56b926321d9600ddbc
Michael Lobato <mike.lobato.wft@gmail.com>
2023-03-29 19:04:26 -0700

    Add: activityRepo file and class

---
+++ src/activityRepository.js
@@ -0,0 +1,10 @@
+class activityRepo {
+    constructor(activityData, userData) {
+        this.activityData = activityData
+        this.userData = userData
+    }
+}
+
+module.exports = activityRepo
+
+
