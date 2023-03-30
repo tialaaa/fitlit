fc0a2e7eaed15a66fb9f50c6ac2b7c7078043a54
Michael Lobato <mike.lobato.wft@gmail.com>
2023-03-29 19:16:01 -0700

    Add: imports, describe, and before each for testing

---
+++ test/activityReop-test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import UserActivity from "../src/activityRepository"
+
+describe('UserActivity', () => {
+    let activityStats, userStats, activityRepo
+    beforeEach(() => {
+        userData = []
+    })
+})
