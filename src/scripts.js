57de07eb729bca311dcd7227c7f2b7432eeaf3d2
Tiala Young <tialayoung@gmail.com>
2023-04-03 11:55:17 -0600

    Adjust stepGoalChart sizing to fit within container

--- src/scripts.js
+++ src/scripts.js
@@ -131,7 +131,7 @@ function renderUserInfo() {
           },
         },
         legend: {
-          position: 'bottom',
+          position: 'right',
           reverse: 'true',
         },
       },
