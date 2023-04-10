22271b7fbc0c2ad95cbb1d37eb34db1a8ee078dd
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-04-10 16:51:30 -0700

    shwos the graphs stored in grtaphFunctionJS and being imported/invoked into the main

--- src/scripts.js
+++ src/scripts.js
@@ -107,36 +107,6 @@ function renderUserInfo() {
 
   displayFriendData(randomId);
   stepGoalChart('stepGoalChart', 'polarArea', randomUser, allUsers.calcAvgStepGoal(), 'red', 'blue')
-  // new Chart(document.getElementById('stepGoalChart'), {
-  //   type: 'polarArea',
-  //   data: {
-  //     labels: ['Your Goal', 'Average User'],
-  //     datasets: [{
-  //       data: [randomUser.dailyStepGoal, allUsers.calcAvgStepGoal()],
-  //       backgroundColor: [
-  //         'rgb(57, 64, 233)',
-  //         'rgb(201, 203, 207)',
-  //       ]
-  //     }],
-  //   },
-  //   options: {
-  //     plugins: {
-  //       title: {
-  //         display: true,
-  //         position: 'top',
-  //         text: 'Daily Step Goal',
-  //         color: 'black',
-  //         font: {
-  //           size: 14,
-  //         },
-  //       },
-  //       legend: {
-  //         position: 'right',
-  //         reverse: 'true',
-  //       },
-  //     },
-  //   }
-  // });
 };
 
 function renderHydration() {
@@ -146,25 +116,6 @@ function renderHydration() {
 
   dailyHydraDom.innerText = `${allHydration.userHydrationByDate(allHydration.hydrationData[0].date, randomId)}`;
   hydrationGraph('weekHydraChart', 'line', weekDays, drank, 'rgb(31, 155, 205')
-  // new Chart(document.getElementById("weekHydraChart"), {
-  //   type: 'line',
-  //   data: {
-  //     labels: [weekDays[0],weekDays[1],weekDays[2],weekDays[3],weekDays[4],weekDays[5],weekDays[6]],
-  //     datasets: [{ 
-  //         data: [drank[0],drank[1],drank[2],drank[3],drank[4],drank[5],drank[6]],
-  //         label: "Ounces Drank",
-  //         borderColor: 'rgb(31, 155, 205)',
-  //         fill: false
-  //       }
-  //     ]
-  //   },
-  //   options: {
-  //     title: {
-  //       display: true,
-  //       text: 'Ounces of water drank per day!'
-  //     }
-  //   }
-  // });
 };
 
 function renderSleep() {
@@ -179,40 +130,6 @@ function renderSleep() {
   averageHours.innerText = `${allSleep.calcAvgDailyHours(randomId)}`;
   averageQuality.innerText = `${allSleep.calcAvgSleepQuality(randomId)}`;
   sleepGraph('weekSleepChart', 'bar', arrayOfHours, arrayOfQuality)
-  // new Chart(document.getElementById("weekSleepChart"), {
-  //   type: 'bar',
-  //   data: {
-  //     labels: [arrayOfHours[0][0],arrayOfHours[1][0],arrayOfHours[2][0],arrayOfHours[3][0],arrayOfHours[4][0],arrayOfHours[5][0],arrayOfHours[6][0]],
-  //     datasets: [{ 
-  //         data: [arrayOfHours[0][1],arrayOfHours[1][1],arrayOfHours[2][1],arrayOfHours[3][1],arrayOfHours[4][1],arrayOfHours[5][1],arrayOfHours[6][1]],
-  //         label: "Hours Slept",
-  //         backgroundColor: 'rgb(141, 22, 233)',
-  //         borderColor: "rgb(203 149 243)",
-  //         fill: false
-  //       },
-  //   { 
-  //     data: [arrayOfQuality[0][1],arrayOfQuality[1][1],arrayOfQuality[2][1],arrayOfQuality[3][1],arrayOfQuality[4][1],arrayOfQuality[5][1],arrayOfQuality[6][1]],
-  //     label: "Sleep Quality",
-  //     backgroundColor: 'grey',
-  //     borderColor: "lightgrey",
-  //     fill: false
-  //   }]
-  //  },
-  //   options: {
-  //     title: {
-  //       display: true,
-  //       text: 'Ounces of water drank per day!',
-  //       scales: {
-  //         xAxes: [{
-  //           stacked: true,
-  //         }],
-  //         yAxes: [{
-  //           stacked: true
-  //         }]
-  //       }
-  //     }
-  //   }
-  // });
 };
 
 function renderActivityInfo() {
@@ -224,24 +141,6 @@ function renderActivityInfo() {
   dailyMilWalked.innerText = `${allActivity.dailyMilesWalked(randomId, allActivity.activityData[randomId].date)}`;
   dailyMinAct.innerText = `${allActivity.dailyMinActive(randomId, allActivity.activityData[randomId].date)}`;
   activityChart('weeklyActChart', 'line', actWeekDates, actWeekSteps, randomUser)
-  // new Chart(document.getElementById("weeklyActChart"), {
-  //   type: 'line',
-  //   data: {
-  //     labels: [actWeekDates[0],actWeekDates[1],actWeekDates[2],actWeekDates[3],actWeekDates[4],actWeekDates[5],actWeekDates[6]],
-  //     datasets: [{ 
-  //         data: [actWeekSteps[0],actWeekSteps[1],actWeekSteps[2],actWeekSteps[3],actWeekSteps[4],actWeekSteps[5],actWeekSteps[6]],
-  //         label: "Steps Walked",
-  //         borderColor: "rgb(57, 64, 233)",
-  //         fill: false
-  //       }, {
-  //         data: [randomUser.dailyStepGoal,randomUser.dailyStepGoal,randomUser.dailyStepGoal,randomUser.dailyStepGoal,randomUser.dailyStepGoal,randomUser.dailyStepGoal,randomUser.dailyStepGoal],
-  //         label: "Daily Step Goal",
-  //         borderColor: "darkgray",
-  //         fill: false,
-  //       }
-  //     ]    
-  //   }
-  // });
 };
 
 function weeklyActivityObject(id, startDate) {
