cf8b2ea00c5b36ca3189f1c3c7a95182c148b1bd
Sara Park <sspark106@gmail.com>
2023-04-11 20:06:51 -0600

    refac: Add semicolons

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,6 @@
 
 function fetchData(type) {
-    return fetch(`http://localhost:3001/api/v1/${type}`)
+    return fetch(`https://fitlit-api.herokuapp.com/api/v1/${type}`)
     .then(res => res.json())
 }
 
