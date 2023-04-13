0cf4bfa581b8ac2b010918cd986c158163453053
Michael Lobato <mike.lobato.wft@gmail.com>
2023-04-13 09:59:34 -0700

    Add: errors if unable to retrieve or post data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,6 +2,7 @@
 function fetchData(type) {
     return fetch(`http://localhost:3001/api/v1/${type}`)
     .then(res => res.json())
+    .catch(err => alert('Unable to retrieve data, try refreshing'))
 }
 
 function postHydration(inputObject) {
@@ -13,7 +14,17 @@ function postHydration(inputObject) {
             "Content-Type": "application/JSON"
         }
     })
-    .then(res => res.json());
-}
+    .then(res => {
+        if(!res.ok && res.status === 422) {
+            throw new Error('Fill out all input fields!')
+        } else if (!res.ok) {
+            throw new Error('Error:' + res.statusText)
+        }   
+        return res.json()
+    })
+    .catch(err => {
+        console.error(err)
+    })
+} 
 
 export { fetchData, postHydration }
